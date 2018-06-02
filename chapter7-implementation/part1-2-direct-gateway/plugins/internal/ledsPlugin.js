d762fb90deec5fd7cccc5934663989e2934e74c0
jlewisoakes <jlewisoakes@gmail.com>
2018-06-03 00:14:58 +0100

    Update

--- chapter7-implementation/part1-2-direct-gateway/plugins/internal/ledsPlugin.js
+++ chapter7-implementation/part1-2-direct-gateway/plugins/internal/ledsPlugin.js
@@ -7,8 +7,7 @@ var localParams = {'simulate': false, 'frequency': 2000};
 
 exports.start = function (params) {
   localParams = params;
-  observe(model); //#A
-
+    
   if (localParams.simulate) {
     simulate();
   } else {
@@ -25,14 +24,7 @@ exports.stop = function () {
   console.info('%s plugin stopped!', pluginName);
 };
 
-function observe(what) {
-  Object.observe(what, function (changes) {
-    console.info('Change detected by plugin for %s...', pluginName);
-    switchOnOff(model.value); //#B
-  });
-};
-
-function switchOnOff(value) {
+exports.switchOnOff(value) {
   if (!localParams.simulate) {
     actuator.write(value === true ? 1 : 0, function () { //#C
       console.info('Changed value of %s to %s', pluginName, value);
